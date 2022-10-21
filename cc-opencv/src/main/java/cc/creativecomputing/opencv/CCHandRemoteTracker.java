75e5c99919be7400902d0ed82424a2c5b2053049
ISMASTERS\frauansible <a>
2022-10-21 22:23:31 +0200

    Flipping etc

--- cc-opencv/src/main/java/cc/creativecomputing/opencv/CCHandRemoteTracker.java
+++ cc-opencv/src/main/java/cc/creativecomputing/opencv/CCHandRemoteTracker.java
@@ -5,172 +5,213 @@ import java.net.DatagramSocket;
 import java.net.SocketException;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.List;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import cc.creativecomputing.app.modules.CCAnimator;
 import cc.creativecomputing.core.CCProperty;
+import cc.creativecomputing.core.events.CCListenerManager;
 import cc.creativecomputing.core.logging.CCLog;
 import cc.creativecomputing.graphics.CCDrawMode;
 import cc.creativecomputing.graphics.CCGraphics;
+import cc.creativecomputing.graphics.texture.CCTexture;
 import cc.creativecomputing.math.CCColor;
 import cc.creativecomputing.math.CCMath;
 import cc.creativecomputing.math.CCVector2;
 import cc.creativecomputing.math.CCVector3;
+import cc.creativecomputing.opencv.CCHandTracker.CCFixedTipEvent;
+import cc.creativecomputing.opencv.CCHandTracker.CCHandInfo;
+import cc.creativecomputing.opencv.filtering.CCFlip;
 
 
-public class CCHandRemoteTracker extends CCHandTracker {
-	
+public class CCHandRemoteTracker {
+
 	@CCProperty(name = "rotangle", min = 0, max = 3.14)
 	protected double _cRotAngle = 0;
 	
-	private CCHandInfo theLastHand = new CCHandInfo();
-	DatagramSocket ds; 
-	byte[] receiveBuffer = new byte[65535];
-    boolean running = true;
-    int w = 1440;
-    int h = 1080;
-	JsonParser parser = new JsonParser();
-	protected ArrayList<CCHandInfo> _myHands  = new ArrayList<CCHandInfo>();
-	String[] fingerNames = new String[] {"THUMB","INDEX_FINGER", "MIDDLE_FINGER", "RING_FINGER", "PINKY"};
-    Runnable receiver = new Runnable() {
-		DatagramPacket packet = new DatagramPacket(receiveBuffer, receiveBuffer.length);
- 	
-		@Override
-		public void run() {
-			
-			while(running) {
-			try {
-				
-				ds.receive(packet);
-				String d = new String(packet.getData()).substring(packet.getOffset(), packet.getLength()).trim().strip();
-			
-				JsonObject handData = parser.parse(d).getAsJsonObject();
-				
-				System.out.println(handData.toString());
-		
-				double indX = handData.get("INDEX_FINGER_TIP").getAsJsonArray().get(0).getAsDouble();
-				double indY = handData.get("INDEX_FINGER_TIP").getAsJsonArray().get(1).getAsDouble();
-				indX = indX * w;
-				indY = indY * h;
-				
-				CCHandInfo hand = new CCHandInfo();
-				hand.center.x = handData.get("WRIST").getAsJsonArray().get(0).getAsDouble();;
-				hand.center.y = handData.get("WRIST").getAsJsonArray().get(0).getAsDouble();;
-				
-				hand.tip.x = indX;
-				hand.tip.y = indY;
-				hand.isHand = true;
-				hand.fingerTips = new ArrayList<CCVector3>();
-				for (String finger : fingerNames) {
-					double x = handData.get(finger+"_TIP").getAsJsonArray().get(0).getAsDouble();
-					double y = handData.get(finger+"_TIP").getAsJsonArray().get(1).getAsDouble();
-					double z = handData.get(finger+"_TIP").getAsJsonArray().get(2).getAsDouble();
-					hand.fingerTips.add(new CCVector3(w*x,h*y,z).rotate(0, 0, 1, _cRotAngle));							
-				}
-				//hand.validFrames = 10;
-				_myHands.clear();
-				updateHandInfo(theLastHand, hand);
-				_myHands.add(theLastHand);
-			
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		}
-	};
-    		
-	Thread receiverThread = new Thread(receiver); 
-    
-	public CCHandRemoteTracker(CCCVVideoIn theVideoIn, Path theMaskTexture) {
-		super(theVideoIn, theMaskTexture);
-		try {
-			ds = new DatagramSocket(1234);
-		
-		} catch (SocketException e) {
-			e.printStackTrace();
-		}
+	@CCProperty(name = "tip smooth", min = 0, max = 1)
+	protected double _cTipSmooth = 0;
+	@CCProperty(name = "jitter smooth", min = 0, max = 1)
+	protected double _cJitterSmooth = 0;
 
-		receiverThread.start();
-	}
+	@CCProperty(name = "min rest time", min = 0, max = 200)
+	protected double _cMinRestFrames = 30;
+	@CCProperty(name = "min size")
+	private double _cMinSize = 2;
+	@CCProperty(name = "max jitter", min = 0, max = 100)
+	protected double _cMaxJitter = 30;
+	@CCProperty(name = "min valid frames", min = 0, max = 100)
+	protected double _cMinValidFrames = 20;
+
+	@CCProperty(name = "progress angle", min = 0, max = 360)
+	protected double _cProgressAngle = 0;
+
+	@CCProperty(name = "selection radius", min = 0, max = 50)
+	protected double _cSelectionRadius = 20;
+	@CCProperty(name = "line strength", min = 0, max = 50)
+	protected double _cLineStrength = 0;
+	@CCProperty(name = "line color")
+	protected CCColor _cLineColor = new CCColor();
+	@CCProperty(name = "progress strength", min = 0, max = 50)
+	protected double _cProgressStrength = 0;
+	@CCProperty(name = "progress color")
+	protected CCColor _cProgressColor = new CCColor();
+
+	@CCProperty(name = "draw selection")
+	private boolean _cDrawSelection = false;
 
 	@CCProperty(name = "draw finger tips")
 	private boolean _cDrawFingerTip = true;
 	@CCProperty(name = "tip radius")
 	private double _cTipRadius = 10;
 	
+	@CCProperty(name = "vflip")
+	private boolean _cVFlip = false;
+	
+	@CCProperty(name = "hflip")
+	private boolean _cHFlip = false;
+	
+	private boolean _myIsInDebug = false;
+	private boolean _myIsInConfig = false;
+	
+	int w = 1440;
+	int h = 1080;
+	final int PORT = 1234;
+	
+	public CCListenerManager<CCFixedTipEvent> fixedTipEvents = CCListenerManager.create(CCFixedTipEvent.class);
+	private CCHandInfo theLastHand = new CCHandInfo();
+
+	protected ArrayList<CCHandInfo> _myHands  = new ArrayList<CCHandInfo>();
+	String[] fingerNames = new String[] {"THUMB","INDEX_FINGER", "MIDDLE_FINGER", "RING_FINGER", "PINKY"};
+
+	
+	Runnable receiver = new Runnable() {
+		boolean running = true;
+		JsonParser parser = new JsonParser();
+		byte[] receiveBuffer = new byte[65535];
+
+		DatagramPacket packet = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+
+		@Override
+		public void run() {
+			DatagramSocket ds;
+			try {
+				ds = new DatagramSocket(PORT);
+
+				while(running) {
+					try {
+						ds.receive(packet);
+						String d = new String(packet.getData()).substring(packet.getOffset(), packet.getLength()).trim().strip();
+
+						JsonObject handData = parser.parse(d).getAsJsonObject();
+
+						System.out.println(handData.toString());
+					
+						CCHandInfo hand = new CCHandInfo();
+						hand.isHand = true;
+						
+						hand.tip.x = mapX(handData.get("INDEX_FINGER_TIP").getAsJsonArray().get(0).getAsDouble());
+						hand.tip.y = mapY(handData.get("INDEX_FINGER_TIP").getAsJsonArray().get(1).getAsDouble());
+					
+						hand.center.x = handData.get("WRIST").getAsJsonArray().get(0).getAsDouble() * w;
+						hand.center.y = handData.get("WRIST").getAsJsonArray().get(0).getAsDouble() * h;
+						
+						hand.fingerTips = new ArrayList<CCVector3>();
+						for (String finger : fingerNames) {
+							double x = handData.get(finger+"_TIP").getAsJsonArray().get(0).getAsDouble();
+							double y = handData.get(finger+"_TIP").getAsJsonArray().get(1).getAsDouble();
+							double z = handData.get(finger+"_TIP").getAsJsonArray().get(2).getAsDouble();
+							hand.fingerTips.add(new CCVector3(w*x,h*y,z).rotate(0, 0, 1, _cRotAngle));							
+						}
+						
+						_myHands.clear();
+						updateHandInfo(theLastHand, hand);
+						_myHands.add(theLastHand);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			} catch (SocketException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		};
+	};
+
+	private double mapX(double x) {
+		return _cHFlip ? w-(x * w) : x*w ;
+	}
+	
+	private double mapY(double y) {
+		return _cVFlip ? h-(y * h) : y*h ;
+	}
+	
+	Thread receiverThread = new Thread(receiver); 
+	public CCHandRemoteTracker(CCCVVideoIn theVideoIn, Path theMaskTexture) {
+		receiverThread.start();
+	}
+
+
+
 	private void drawFingerTips(CCHandInfo myInfo, CCGraphics g) {
 		//if(!_cDrawFingerTip)return;
-
 		g.color(CCColor.MAGENTA, 0.5);
-		
 		myInfo.fingerTips.forEach(myTip -> g.ellipse(myTip.xy(),_cTipRadius,_cTipRadius, false));
-		
 		if(myInfo.tip.isZero())return;
-		
 		g.color(CCColor.CYAN, 0.5);
 		g.ellipse(myInfo.tip,_cTipRadius,_cTipRadius, false);
 	}
-	
+
 	private void updateHandInfo(CCHandInfo theLastHand, CCHandInfo theNewHand) {
 		theLastHand.isCorrelated = true;
-    	
-		// update cursor with new position
+
 		theLastHand.handContour = theNewHand.handContour;
 		theLastHand.simpleContour = theNewHand.simpleContour;
 		theLastHand.fingerTips = theNewHand.fingerTips;
 		theLastHand.startAngle = theNewHand.startAngle;
-			
+
 		theLastHand.center.x = theNewHand.center.x * (1 - _cTipSmooth) + theLastHand.center.x * _cTipSmooth;
 		theLastHand.center.y = theNewHand.center.y * (1 - _cTipSmooth) + theLastHand.center.y * _cTipSmooth;
 
 		double myJitter = theNewHand.tip.distance(theLastHand.tip);
-		
-		if(theNewHand.tip.isZero()) {
-//			CCLog.info(theNewHand.tip.isZero(),theLastHand.tip);
-		//	return;
-		}else {
+
+		if(!theNewHand.tip.isZero()) {
 			theLastHand.tip.x = theNewHand.tip.x * (1 - _cTipSmooth) + theLastHand.tip.x * _cTipSmooth;
 			theLastHand.tip.y = theNewHand.tip.y * (1 - _cTipSmooth) + theLastHand.tip.y * _cTipSmooth;
 		}
-			
+
 		theLastHand.jitter = myJitter * (1 - _cJitterSmooth) + theLastHand.jitter * _cJitterSmooth;
 		theLastHand.validFrames++;
-				
+
 		double myLastRestTime = theLastHand.restFrames;
-		System.out.println(theLastHand.jitter);
 		if(theLastHand.jitter < _cMaxJitter && !theLastHand.tip.isZero()) {
-		
 			theLastHand.restFrames++;
-		}
-		else {
+		} else {
 			theLastHand.restFrames = 0;
 		}
-				
+
 		theLastHand.progress = CCMath.saturate(theLastHand.restFrames / _cMinRestFrames);
-		System.out.println(theLastHand.progress);
 		if(myLastRestTime < _cMinRestFrames && theLastHand.restFrames >= _cMinRestFrames) {
-//			if(_myBoundingPolygon== null || _myBoundingPolygon.vertices().size() < 3 || _myBoundingPolygon.isInShape(theLastHand.tip)) {
-				fixedTipEvents.proxy().event(theLastHand.tip);
-//			}
+			fixedTipEvents.proxy().event(theLastHand.tip);
 		}
 	}
-	
+
 	public void drawDebug(CCGraphics g) {
 		g.color(255);
-		g.pushMatrix();
-		g.applyTransform(transform());
-		g.popMatrix();
 		for(CCHandInfo myInfo:_myHands) {
 			drawFingerTips(myInfo, g);
 		}
 
-		//if(_cDrawSelection)drawSelection(g);
+		if(_cDrawSelection)drawSelection(g);
 	}
 	
-	public void drawSelection(CCGraphics g) {
+	public List<CCHandInfo> hands(){
+		return _myHands;
+	}
 
+	public void drawSelection(CCGraphics g) {
 		for(CCHandInfo myInfo:_myHands) {
 			if(myInfo.tip.isZero())continue;
 			if(myInfo.validFrames < _cMinValidFrames) {
@@ -178,10 +219,10 @@ public class CCHandRemoteTracker extends CCHandTracker {
 				continue;
 			}
 			g.color(1d);
-			
+
 			CCVector2 myDirection = myInfo.tip.subtract(myInfo.center).normalizeLocal();
 			double startAngle = CCMath.atan2(-myDirection.y, myDirection.x);
-			
+
 			g.color(_cLineColor);
 			g.beginShape(CCDrawMode.TRIANGLE_STRIP);
 			for(double a = _cProgressAngle * myInfo.progress; a < _cProgressAngle;a++) {
@@ -190,11 +231,10 @@ public class CCHandRemoteTracker extends CCHandTracker {
 				g.vertex(CCVector2.circlePoint(myAngle, _cSelectionRadius + _cLineStrength / 2, myInfo.tip.x, myInfo.tip.y));
 			}
 			g.endShape();
-			
+
 			g.color(_cProgressColor);
 			if(myInfo.progress>= 1)g.color(CCColor.CYAN);
 			g.beginShape(CCDrawMode.TRIANGLE_STRIP);
-			System.out.println("P "+myInfo.progress);
 			for(double a = 0; a < _cProgressAngle * myInfo.progress;a++) {
 				double myAngle = CCMath.radians(a) - startAngle - CCMath.radians(_cProgressAngle / 2);// - CCMath.HALF_PI;
 				g.vertex(CCVector2.circlePoint(myAngle, _cSelectionRadius - _cProgressStrength / 2, myInfo.tip.x, myInfo.tip.y));
@@ -206,5 +246,36 @@ public class CCHandRemoteTracker extends CCHandTracker {
 	}
 	
 	
+	public void isInDebug(boolean isInDebug) {
+		_myIsInDebug = isInDebug;
+	}
+	
+	public void isInConfig(boolean isInConfig) {
+		_myIsInConfig = isInConfig;
+	}
 
+	public void active(boolean _myIsTrackingActive) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateDebugTexture(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void update(CCAnimator theAnimator) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public CCTexture debugTexture() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void preDisplay(CCGraphics g) {
+		// TODO Auto-generated method stub
+		
+	}
 }
